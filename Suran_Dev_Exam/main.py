1f98fe13b81d40ad6df16d8247a8c8ac81c34b37
HarpersDad <42348662+HarpersDad@users.noreply.github.com>
2021-10-06 12:48:35 -0400

    Sort text assignment for CSC 499
    Suran Dev sort text assignment

---
+++ Suran_Dev_Exam/main.py
@@ -0,0 +1,41 @@
+# Clifton Lindsey
+# CSC 499 Career Prep
+# October 6, 2021
+# written on MacOS Big Sur so hopefully it will run in linux
+
+# function for sort
+def sort(fileIn):
+
+    # read in the text file
+    inFile = open(fileIn)
+    text = []
+
+    # for loop that adds the names/words to the text array
+    for words in inFile:
+        temp = words.split()
+        for i in temp:
+            text.append(i)
+
+    # closes the read in document
+    inFile.close()
+
+    # sorts the text that was read in alphabetically
+    text.sort()
+
+    # sorts the text by length of name/word and sets the result as an array
+    sortedText = sorted(text, key = len)
+
+    # creates word document that will be wrote to and sets it as a variable
+    fileOut = open("output.txt", "w")
+
+    # for loop to iterate through the sorted array and write it to the output file
+    for i in sortedText:
+        fileOut.writelines(i)
+        fileOut.writelines("\n")
+
+    # closes the output file
+    fileOut.close()
+
+# function call
+sort("Sort Me.txt")
+
